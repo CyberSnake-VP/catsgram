b6a568d118b680624e1ae0c3342185ce3c6d110c
Vadim <zigmundfreid89@yandex.ru>
2026-04-18 23:40:08 +0300

    развернута бд в compose.yaml

--- src/main/java/example/CatsgramApp.java
+++ src/main/java/example/CatsgramApp.java
@@ -1,9 +1,11 @@
 package example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class CatsgramApp {
     public static void main(String[] args) {
-
-
+        SpringApplication.run(CatsgramApp.class, args);
     }
 }
