868e4bb54febff9939f329e3a12203b883f59997
Vadim <zigmundfreid89@yandex.ru>
2026-04-19 16:04:29 +0300

    Закончен Controller для User

--- src/main/java/example/controller/UserController.java
+++ src/main/java/example/controller/UserController.java
@@ -1,12 +1,15 @@
 package example.controller;
 
 import example.dto.request.user.CreateUserRequest;
+import example.dto.request.user.UpdateUserRequest;
 import example.dto.response.user.UserResponse;
 import example.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -19,5 +22,24 @@ public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     public UserResponse create( @Valid @RequestBody CreateUserRequest request) {
         return userService.create(request);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserResponse getById(@PathVariable Long id) {
+        return userService.getById(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserResponse> getAll() {
+        return userService.getAll();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserResponse update(@PathVariable Long id,
+                               @Valid @RequestBody UpdateUserRequest request) {
+        return userService.update(id, request);
     }
 }
