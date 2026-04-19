5ccb9f2f0eac67027a0a2830703bc16de9cd3021
Vadim <zigmundfreid89@yandex.ru>
2026-04-19 13:24:08 +0300

    Реализован UserRowMapper

--- src/main/java/example/dal/mappers/UserRowMapper.java
+++ src/main/java/example/dal/mappers/UserRowMapper.java
@@ -6,12 +6,21 @@ import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 
 @Component
 public class UserRowMapper implements RowMapper<User> {
 
     @Override
     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return null;
+        User user = new User();
+        user.setId(rs.getLong("id"));
+        user.setUsername(rs.getString("username"));
+        user.setPassword(rs.getString("password"));
+        user.setEmail(rs.getString("email"));
+
+        Timestamp registrationDate = rs.getTimestamp("registration_date");
+        user.setRegistrationDate(registrationDate.toInstant());
+        return user;
     }
 }
