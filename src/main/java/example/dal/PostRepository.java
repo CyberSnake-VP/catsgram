7cbd46c261109ead37cea93454ce99d046f2b1c0
Vadim <zigmundfreid89@yandex.ru>
2026-04-19 17:13:23 +0300

    Законен RowMapper, и CRUD repository

--- src/main/java/example/dal/PostRepository.java
+++ src/main/java/example/dal/PostRepository.java
@@ -1,10 +1,54 @@
 package example.dal;
 
-import lombok.RequiredArgsConstructor;
+import example.model.Post;
+import example.model.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
 
 
-@RequiredArgsConstructor
-public class PostRepository {
+@Repository
+public class PostRepository extends BaseRepository<Post> {
+    public PostRepository(JdbcTemplate jdbc, RowMapper<Post> rowMapper) {
+        super(jdbc, rowMapper);
+    }
+
+    private static final String INSERT_QUERY = "INSERT INTO posts (author_id, description, post_date) " +
+            "VALUES (?, ?, ?) RETURNING id";
+    private static final String GET_BY_ID_QUERY = "SELECT * FROM posts WHERE id = ?";
+    private static final String GET_ALL = "SELECT * FROM posts";
+    private static final String UPDATE_QUERY = "UPDATE posts SET description = ? WHERE id = ?";
 
 
+    public Post create(Post post) {
+        long id = insert(
+                INSERT_QUERY,
+                post.getAuthorId(),
+                post.getDescription(),
+                Timestamp.from(post.getPostDate())
+        );
+        post.setId(id);
+        return post;
+    }
+
+    public Optional<Post> findById(Long id) {
+        return findOne(GET_BY_ID_QUERY, id);
+    }
+
+    public List<Post> findAll() {
+        return findAll(GET_ALL);
+    }
+
+    public Post update(Post post) {
+        update(
+                UPDATE_QUERY,
+                post.getDescription(),
+                post.getId()
+        );
+        return post;
+    }
 }
--- src/main/java/example/dal/mappers/PostRowMapper.java
+++ src/main/java/example/dal/mappers/PostRowMapper.java
@@ -2,14 +2,21 @@ package example.dal.mappers;
 
 import example.model.Post;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Component
 public class PostRowMapper implements RowMapper<Post> {
 
     @Override
     public Post mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return null;
+        Post post = new Post();
+        post.setId(rs.getLong("id"));
+        post.setAuthorId(rs.getLong("author_id"));
+        post.setDescription(rs.getString("description"));
+        post.setPostDate(rs.getDate("post_date").toInstant());
+        return post;
     }
 }
