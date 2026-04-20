3b315eb6d2a1e3d834601641162f056e615e0dc4
Vadim <zigmundfreid89@yandex.ru>
2026-04-20 13:42:48 +0300

    Bag-fix PostRowMapper

--- src/main/java/example/CatsgramApp.java
+++ src/main/java/example/CatsgramApp.java
@@ -1,11 +1,15 @@
 package example;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class CatsgramApp {
     public static void main(String[] args) {
-        SpringApplication.run(CatsgramApp.class, args);
+       SpringApplication.run(CatsgramApp.class, args);
     }
 }
---
+++ src/main/java/example/dto/response/image/ImageData.java
@@ -0,0 +1,4 @@
+package example.dto.response.image;
+
+public class ImageData {
+}
